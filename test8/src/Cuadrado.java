960770fae6b18a5670a139c0da66875db1ddfc3e
lamDAW69 <118375353+lamDAW69@users.noreply.github.com>
2024-10-14 17:26:55 +0200

    Primeras estructuras de POO, objetos, getters y setters

---
+++ test8/src/Cuadrado.java
@@ -0,0 +1,14 @@
+public class Cuadrado {
+    private double lado;
+
+    public Cuadrado(double lado) {      //Constructor
+        this.lado = lado;
+    }
+    public void setLado(double lado) { //Setter
+        this.lado = lado;
+    }
+
+    public double getLado() {        //Getter
+        return lado;
+    }
+}
--- test8/src/Main.java
+++ test8/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);*/
+
+        //Crear un objeto cuadrado con un lado de 5.6
+
+        Cuadrado square = new Cuadrado(5.6);
+        System.out.println("El lado del cuadrado es: " + square.getLado());
+
     }
 }
+
+//There's still no relevant content to start pushing changes to this repo
+//Still not relevant content at least for now, we were taking action over dates and how to parse them.
