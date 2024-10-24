420718ad795cc42bc43897268d8b0a5028e31452
lamDAW69 <118375353+lamDAW69@users.noreply.github.com>
2024-10-24 18:44:39 +0200

    Anotaciones de LOMBOK para entidades al igual que el crud básico

--- DAO_lombok/src/main/java/org/example/dao/puntuacionesDAOMySql.java
+++ DAO_lombok/src/main/java/org/example/dao/puntuacionesDAOMySql.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class puntuacionesDAOMySql implements puntuacionesDAO {
 
-    private final String url = "jdbc:mysql://localhost:3306/puntuaciones";
+    private final String db = "jdbc:mariadb://localhost:3306/puntuaciones";
     private final String user = "root";
     private final String pass ="";
 
@@ -19,13 +19,13 @@ public class puntuacionesDAOMySql implements puntuacionesDAO {
 
         try (Connection conn = DriverManager.getConnection(db, user, pass)) {
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM puntuaciones");
+            ResultSet rs = st.executeQuery("SELECT * FROM puntuacion");
 
             while(rs.next()) {
                 puntuaciones.add(new puntuaciones(rs.getString("jugador"), rs.getInt("puntuacion")));
             }
         }   catch(SQLException e) {
-            e.getMessage();
+            System.err.println(e.getMessage());
         }
 
         return puntuaciones;
@@ -35,12 +35,12 @@ public class puntuacionesDAOMySql implements puntuacionesDAO {
     public void insertPuntuacion(puntuaciones puntuacion) {
 
         try(Connection conn = DriverManager.getConnection(db, user, pass)) {
-            PreparedStatement ps = conn.prepareStatement(("INSERT INTO puntuaciones (jugador, puntuacion VALUES (?), (?))"));
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO puntuacion (jugador, puntuacion) VALUES (?, ?)");
             ps.setString(1, puntuacion.getJugador());
             ps.setInt(2, puntuacion.getPuntuacion());
             ps.executeUpdate();
         } catch(SQLException e) {
-            e.getMessage();
+            System.err.println(e.getMessage());
         }
 
     }
@@ -48,23 +48,23 @@ public class puntuacionesDAOMySql implements puntuacionesDAO {
     @Override
     public void deletePuntuacion(puntuaciones puntuacion) {
         try(Connection conn = DriverManager.getConnection(db, user, pass)) {
-            PreparedStatement ps = conn.prepareStatement("DELETE FROM puntuaciones WHERE jugador = ?");
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM puntuacion WHERE jugador = ?");
             ps.setString(1, puntuacion.getJugador());
             ps.executeUpdate();
         } catch(SQLException e) {
-            e.getMessage();
+            System.err.println(e.getMessage());
         }
     }
 
     @Override
     public void updatePuntuacion(puntuaciones puntuacion) {
         try(Connection conn = DriverManager.getConnection(db, user, pass)) {
-            PreparedStatement ps = conn.prepareStatement("UPDATE puntuaciones SET puntuacion = ? WHERE jugador = ?");
+            PreparedStatement ps = conn.prepareStatement("UPDATE puntuacion SET puntuacion = ? WHERE jugador = ?");
             ps.setInt(1, puntuacion.getPuntuacion());
             ps.setString(2, puntuacion.getJugador());
             ps.executeUpdate();
         } catch(SQLException e) {
-            e.getMessage();
+            System.err.println(e.getMessage());
         }
     }
 }
