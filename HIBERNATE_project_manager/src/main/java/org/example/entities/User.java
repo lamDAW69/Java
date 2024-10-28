9e5d092a27d54535cb0d617ecf0573b634cf4c54
lamDAW69 <118375353+lamDAW69@users.noreply.github.com>
2024-10-28 18:44:32 +0100

    User Entity

---
+++ HIBERNATE_project_manager/src/main/java/org/example/entities/User.java
@@ -0,0 +1,58 @@
+package org.example.entities;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@NamedQueries({
+        @NamedQuey(name = "User.findAll", query = "SELECT U FROM User U"),
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; // primary key
+
+    @NonNull
+    private String name;
+
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(
+            name = "productos_favoritos",
+            joinColumns = @JoinColumn(name = "idUsuario"),
+            inverseJoinColumns = @JoinColumn(name = "idProducto")
+    )
+    private Set<Producto> favoritos;
+
+    public User(int id, @NonNull String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String toString() {
+        return String.format("%5d %-40s", id, nombre);
+    }
+
+}
