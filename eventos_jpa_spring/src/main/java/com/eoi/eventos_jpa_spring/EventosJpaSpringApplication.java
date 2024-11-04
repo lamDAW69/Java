5cbd0d7d5f7f60a1f90adcb4e4ab6e5c9f2ab5d7
lamDAW69 <118375353+lamDAW69@users.noreply.github.com>
2024-11-04 18:42:19 +0100

    JPA spring typical cruds new project

---
+++ eventos_jpa_spring/src/main/java/com/eoi/eventos_jpa_spring/EventosJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.eoi.eventos_jpa_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosJpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosJpaSpringApplication.class, args);
+	}
+
+}
---
+++ eventos_jpa_spring/src/test/java/com/eoi/eventos_jpa_spring/EventosJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoi.eventos_jpa_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
